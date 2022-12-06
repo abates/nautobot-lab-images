3a0ddc99983f34a708235ee14bcc09752c5ac82b
Andrew Bates <andrew.bates@networktocode.com>
2022-12-06 10:38:30 -0500

    Fixed receptor/worker name

--- awx/rootfs/usr/local/bin/bootstrap_development.sh
+++ awx/rootfs/usr/local/bin/bootstrap_development.sh
@@ -11,6 +11,6 @@ fi
 awx-manage create_preload_data
 awx-manage register_default_execution_environments
 
-awx-manage provision_instance --hostname="${HOSTNAME}" --node_type="hybrid"
+awx-manage provision_instance --hostname="localhost" --node_type="hybrid"
 awx-manage register_queue --queuename=controlplane --instance_percent=100
 awx-manage register_queue --queuename=default --instance_percent=100
