15a96ce936d43586242fa4daa91c67f77c655e9e
Andrew Bates <andrew.bates@networktocode.com>
2022-12-09 12:30:35 -0500

    Added ability to load fixture data

--- awx/rootfs/usr/local/bin/bootstrap_development.sh
+++ awx/rootfs/usr/local/bin/bootstrap_development.sh
@@ -17,3 +17,14 @@ awx-manage register_queue --queuename=default --instance_percent=100
 if [ $ANSIBLE_TOWER_SAMPLES == "true" ] ; then
   awx-manage create_preload_data
 fi
+
+# Load any fixtures
+AWX_FIXTURES=/opt/awx/fixtures
+if [ -d $AWX_FIXTURES ] ; then
+  for FIXTURE_DIR in `ls $AWX_FIXTURES` ; do
+    for fixture in `ls $AWX_FIXTURES/$FIXTURE_DIR` ; do
+      echo -n "Loading fixture $AWX_FIXTURES/$FIXTURE_DIR/$fixture: "
+      awx-manage loaddata $AWX_FIXTURES/$FIXTURE_DIR/$fixtur
+    done
+  done
+fi
